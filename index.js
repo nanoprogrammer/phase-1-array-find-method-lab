dcdd5c2bec0f254dea25b31246298f04f0c52ffb
Samson Njai <njaisamson@gmail.com>
2024-07-11 07:41:50 +0300

    Adds superbowlWin function.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+  function superbowlWin(array) {
+    for(let win of array) {
+        if (win.result === "W") {
+            return win.year
+        }
+    }
+
+  };
+  superbowlWin(record);
